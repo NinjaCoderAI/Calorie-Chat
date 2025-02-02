2d41e403f0182355d83d8ddb834018015466f9a1
NinjaCoderAI <david@oakbankinnovations.com>
2025-02-02 20:54:55 +0000

    Update user.js

--- project 5 - WhatsAppTemplatewithImageAnalysis/src/models/user.js
+++ project 5 - WhatsAppTemplatewithImageAnalysis/src/models/user.js
@@ -1,12 +1,12 @@
-import { supabase } from '../config/supabase.js';
+const { supabase } = require('../config/supabase');
 
-export class User {
+class User {
   static async create(phone, subscriptionStatus = 'inactive') {
     const { data, error } = await supabase
       .from('users')
       .insert([{ phone, subscription_status: subscriptionStatus }])
       .select();
-    
+
     if (error) throw error;
     return data[0];
   }
@@ -17,7 +17,7 @@ export class User {
       .update({ subscription_status: status })
       .eq('phone', phone)
       .select();
-    
+
     if (error) throw error;
     return data[0];
   }
@@ -28,8 +28,10 @@ export class User {
       .select('*')
       .eq('phone', phone)
       .single();
-    
+
     if (error) throw error;
     return data;
   }
 }
+
+module.exports = User;
